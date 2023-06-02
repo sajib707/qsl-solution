c5d9c80d526c1cf25e281258806a65262b9b9b58
Sajib Mahmud <mdsajibmahmud71@gmail.com>
2023-06-02 09:52:51 +0600

    updated product filtering feature

--- products/urls.py
+++ products/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 app_name = 'products'
 
+
 urlpatterns = [
     path('', views.index),
-    path('index', views.index),
-    path('listproducts/', views.list_products, name='list_products'),
-    path('contact', views.contact),
+    path('list_products/', views.list_products, name='list_products'),
     path('details-<slug:slug>/', views.product_detail, name='product_detail'),
-    path('listproducts/filter-<slug:slug>/', views.list_products, name='product_filter'),
+    path('listproducts/filter-<slug:slug>/', views.product_filter, name='product_filter'),
     path('api/suggestionapi/', views.suggestionApi, name='suggestionapi'),
 ]
+
--- products/views.py
+++ products/views.py
@@ -1,10 +1,8 @@
 
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
-from .models import Product, Category
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .models import Product, Category, ProductType
 from django.db.models import Q
-PRODUCTS_PER_PAGE = 12
 
 
 def categories(request):
@@ -20,70 +18,112 @@ def contact(request):
     return render(request,'products/contact.html')
 
 
-def list_products(request, slug=None):
-
-    ordering = request.GET.get('ordering', "")
-    search = request.GET.get('search', "")
-    brands = set(Product.objects.values_list('brand', flat=True))
-    colors = Product.objects.values_list('color', flat=True).distinct()
-
-    selected_brands = request.GET.getlist('brand')
-    selected_colors = request.GET.getlist('color')
-
-    filtered_products = Product.objects.all()
-
-    if slug:
-        # Handle filtered listing
-        filtered_products = filtered_products.filter(category__slug=slug)
+def list_products(request):
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        ordering = request.GET.get('ordering', "")
+        search = request.GET.get('search', "")
+        selected_brands = request.GET.getlist('brand')
+        selected_warranties = request.GET.getlist('warranty')
+        selected_sellers = request.GET.getlist('seller')
+        selected_colors = request.GET.getlist('color')
+        selected_product_types = request.GET.getlist('product_type')
 
 
-    if selected_brands:
-        q_objects = Q()
-        for brand in selected_brands:
-            q_objects |= Q(brand=brand)
-        filtered_products = filtered_products.filter(q_objects)
+        filtered_products = Product.objects.all()
 
-    if selected_colors:
-        q_objects = Q()
-        for color in selected_colors:
-            q_objects |= Q(color=color)
-        filtered_products = filtered_products.filter(q_objects)
+        if selected_product_types:
+            product_types = ProductType.objects.filter(name__in=selected_product_types)
+            filtered_products = filtered_products.filter(product_type__in=product_types)
+
+
+        if selected_brands:
+            filtered_products = filtered_products.filter(brand__in=selected_brands)
+
+        if selected_warranties:
+            filtered_products = filtered_products.filter(warranty__in=selected_warranties)
+
+        if selected_sellers:
+            filtered_products = filtered_products.filter(seller__in=selected_sellers)
+
+        if selected_colors:
+            filtered_products = filtered_products.filter(color__in=selected_colors)
+
+        if search:
+            filtered_products = filtered_products.filter(Q(title__icontains=search) | Q(brand__icontains=search))
+
+        if ordering:
+            filtered_products = filtered_products.order_by(ordering)
+
+        products_data = []
+        for product in filtered_products:
+            product_data = {
+                'title': product.title,
+                'brand': product.brand,
+                'product_images': [image.image.url for image in product.product_image.all()],
+                'discount_price': product.discount_price,
+                'regular_price': product.regular_price,
+                # Include other fields if needed
+            }
+            products_data.append(product_data)
+
+        return JsonResponse({'products': products_data})
+
+    else:
+        ordering = request.GET.get('ordering', "")
+        search = request.GET.get('search', "")
+        brands = set(Product.objects.values_list('brand', flat=True))
+        warranties = set(Product.objects.values_list('warranty', flat=True))
+        sellers = set(Product.objects.values_list('seller', flat=True))
+        colors = set(Product.objects.values_list('color', flat=True))
+        selected_brands = request.GET.getlist('brand')
+        selected_warranties = request.GET.getlist('warranty')
+        selected_sellers = request.GET.getlist('seller')
+        selected_colors = request.GET.getlist('color')
+        selected_product_types = request.GET.getlist('product_type')
+
+
+        filtered_products = Product.objects.all()
+
+        if selected_product_types:
+            product_types = ProductType.objects.filter(name__in=selected_product_types)
+            filtered_products = filtered_products.filter(product_type__in=product_types)
+
+
+        if selected_brands:
+            filtered_products = filtered_products.filter(brand__in=selected_brands)
+
+        if selected_warranties:
+            filtered_products = filtered_products.filter(warranty__in=selected_warranties)
+
+        if selected_sellers:
+            filtered_products = filtered_products.filter(seller__in=selected_sellers)
+
+        if selected_colors:
+            filtered_products = filtered_products.filter(color__in=selected_colors)
+
+        if search:
+            filtered_products = filtered_products.filter(Q(title__icontains=search) | Q(brand__icontains=search))
+
+        if ordering:
+            filtered_products = filtered_products.order_by(ordering)
+
+        context = {
+            'brands': brands,
+            'warranties': warranties,
+            'sellers': sellers,
+            'colors': colors,
+            'selected_brands': selected_brands,
+            'selected_warranties': selected_warranties,
+            'selected_sellers': selected_sellers,
+            'selected_colors': selected_colors,
+            'selected_product_types': selected_product_types,
+            'products': filtered_products,
+            'product_types': ProductType.objects.all(),
+
+        }
+
+        return render(request, 'products/listproducts.html', context)
     
-    if search:
-        filtered_products = filtered_products.filter(Q(title__icontains=search) | Q(brand__icontains=search))
-
-    if ordering:
-        products = products.order_by(ordering)
-
-
-    context = {
-        'brands': brands,
-        'colors': colors,
-        'selected_brands': selected_brands,
-        'selected_colors': selected_colors,
-        'products': filtered_products,
-    }
-
-
-    # Pagination
-    #page = request.GET.get('page', 1)
-    # product_paginator = Paginator(products, PRODUCTS_PER_PAGE)
-    #try:
-     #   products = product_paginator.page(page)
-    #except EmptyPage:
-     #   products = product_paginator.page(product_paginator.num_pages)
-    #except:
-     #   products = product_paginator.page(PRODUCTS_PER_PAGE)
-
-    return render(request, "products/listproducts.html", context)
-    #{
-     #   "products": products,
-      #  'page_obj': products,
-       # 'is_paginated': True,
-        #'paginator': product_paginator
-    #})
-
-
 
 
 def suggestionApi(request):
@@ -108,7 +148,7 @@ def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, is_active=True)
     return render(request,'products/detail.html',{'product' : product})
 
-def product_filter(request, slug=None):
+def product_filter(request, slug):
     category = get_object_or_404(Category, slug=slug)
     products = Product.objects.filter(category = category)
     return render(request,'products/listproducts.html',{'category' : category, 'products' : products})
